d91222d9363888b010dc04aa01a50f9d8b9b1774
Hunter <muratoreh@gmail.com>
2023-10-02 16:44:07 -0400

    Check that the user inputs a valid color

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const colors = require('colors');
 const fs = require('fs');
-let validColors;
+const generateSVG = require('./utils/generateSVG');
 
 const questions = [
     {
@@ -24,37 +24,171 @@ const questions = [
 
 function init() {
     inquirer.prompt(questions).then((answers) => {
-            userAnswers = answers;
-            showChoiceMenu();
-        });
+        generateSVG(answers);
+    });
 }
 
 function validateText(input) {
-    if (0 < input.length <= 3) {
+    if (input.length > 0 && input.length <= 3) {
         return true;
     }
     
-    return 'Text must be between 0 and 3 characters'.brightRed;
+    return 'Text must be between 0 and 3 characters - try again'.brightRed;
 }
 
 function validateColor(input) {
-    // Read the JSON file containing the array of valid colors and assign the array to a variable
-    fs.readFile('colors.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error('Error reading JSON file:', err);
-          return;
-        }
-
-        validColors = JSON.parse(data).validColors;
-    });
-
-    if (validColors.includes(input)) {
+    if (validColors.includes(input.toLowerCase())) {
         return true;
-    } else if (input.length = 6 && /^[0-9A-Fa-f]+$/.test(input)) {
+    } else if (input.length == 6 && /^[0-9A-Fa-f]+$/.test(input)) {
         return true;
     }
     
-    return 'Must enter a color keyword (i.e. Red) or a valid hexadecimal number (i.e. #a1b2c3'.brightRed;
+    return 'Must enter a color keyword (i.e. Red) or a valid 6 digit hexadecimalnumber (i.e. #a1b2c3) - try again'.brightRed;
 }
 
 init();
+
+validColors = [
+    "transparent",
+    "aliceblue",
+    "antiquewhite",
+    "aqua",
+    "aquamarine",
+    "azure",
+    "beige",
+    "bisque",
+    "black",
+    "blanchedalmond",
+    "blue",
+    "blueviolet",
+    "brown",
+    "burlywood",
+    "cadetblue",
+    "chartreuse",
+    "chocolate",
+    "coral",
+    "cornflowerblue",
+    "cornsilk",
+    "crimson",
+    "cyan",
+    "darkblue",
+    "darkcyan",
+    "darkgoldenrod",
+    "darkgray",
+    "darkgreen",
+    "darkkhaki",
+    "darkmagenta",
+    "darkolivegreen",
+    "darkorange",
+    "darkorchid",
+    "darkred",
+    "darksalmon",
+    "darkseagreen",
+    "darkslateblue",
+    "darkslategray",
+    "darkturquoise",
+    "darkviolet",
+    "deeppink",
+    "deepskyblue",
+    "dimgray",
+    "dodgerblue",
+    "firebrick",
+    "floralwhite",
+    "forestgreen",
+    "fuchsia",
+    "gainsboro",
+    "ghostwhite",
+    "gold",
+    "goldenrod",
+    "gray",
+    "green",
+    "greenyellow",
+    "honeydew",
+    "hotpink",
+    "indianred",
+    "indigo",
+    "ivory",
+    "khaki",
+    "lavender",
+    "lavenderblush",
+    "lawngreen",
+    "lemonchiffon",
+    "lightblue",
+    "lightcoral",
+    "lightcyan",
+    "lightgoldenrodyellow",
+    "lightgray",
+    "lightgreen",
+    "lightpink",
+    "lightsalmon",
+    "lightseagreen",
+    "lightskyblue",
+    "lightslategray",
+    "lightsteelblue",
+    "lightyellow",
+    "lime",
+    "limegreen",
+    "linen",
+    "magenta",
+    "maroon",
+    "mediumaquamarine",
+    "mediumblue",
+    "mediumorchid",
+    "mediumpurple",
+    "mediumseagreen",
+    "mediumslateblue",
+    "mediumspringgreen",
+    "mediumturquoise",
+    "mediumvioletred",
+    "midnightblue",
+    "mintcream",
+    "mistyrose",
+    "moccasin",
+    "navajowhite",
+    "navy",
+    "oldlace",
+    "olive",
+    "olivedrab",
+    "orange",
+    "orangered",
+    "orchid",
+    "palegoldenrod",
+    "palegreen",
+    "paleturquoise",
+    "palevioletred",
+    "papayawhip",
+    "peachpuff",
+    "peru",
+    "pink",
+    "plum",
+    "powderblue",
+    "purple",
+    "rebeccapurple",
+    "red",
+    "rosybrown",
+    "royalblue",
+    "saddlebrown",
+    "salmon",
+    "sandybrown",
+    "seagreen",
+    "seashell",
+    "sienna",
+    "silver",
+    "skyblue",
+    "slateblue",
+    "slategray",
+    "snow",
+    "springgreen",
+    "steelblue",
+    "tan",
+    "teal",
+    "thistle",
+    "tomato",
+    "turquoise",
+    "violet",
+    "wheat",
+    "white",
+    "whitesmoke",
+    "yellow",
+    "yellowgreen"
+]
