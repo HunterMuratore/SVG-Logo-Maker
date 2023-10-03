ff658639a3b91746ca848041c64e0d97573b0640
Hunter <muratoreh@gmail.com>
2023-10-03 17:18:12 -0400

    Add unit testing

---
+++ utils/generateSVG.test.js
@@ -0,0 +1,26 @@
+const generateSVG = require('./generateSVG');
+
+describe('Generate SVG Tests', () => {
+    it('Should return a string', () => {
+        const svg = generateSVG({
+            text: 'HMM',
+            color: 'blue',
+            shape: 'Circle',
+            shapeColor: 'green'
+        });
+
+        expect(typeof svg).toBe('string');
+    });
+
+    it('Should set random colors', () => {
+        const svg = generateSVG({
+            text: 'HMM',
+            color: 'random',
+            shape: 'Circle',
+            shapeColor: 'random'
+        });
+
+        expect(svg.color).not.toEqual('random');
+        expect(svg.shapeColor).not.toEqual('random');
+    });
+})
