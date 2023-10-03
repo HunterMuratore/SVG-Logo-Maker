6609fd2f5016d2755bfb51d2e36cec0ca29862bc
Hunter <muratoreh@gmail.com>
2023-10-03 09:58:50 -0400

    Moved validColors array to json, allow random color selection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const colors = require('colors');
 const fs = require('fs');
 const generateSVG = require('./utils/generateSVG');
+const validColors = require('./colors.json');
 
 const questions = [
     {
@@ -11,7 +12,7 @@ const questions = [
     },
     {
         name: 'color',
-        message: 'Enter a color keyword or a 6 digit hexadecimal number for the text:'.cyan,
+        message: 'Enter a color keyword or a 6 digit hexadecimal number for the text (or try a \'random\' color):'.cyan,
         validate: validateColor
     },
     {
@@ -22,14 +23,22 @@ const questions = [
     },
     {
         name: 'shapeColor',
-        message: 'Enter a color keyword or a 6 digit hexadecimal number for the shape:'.cyan,
+        message: 'Enter a color keyword or a 6 digit hexadecimal number for the shape (or try a \'random\' color):'.cyan,
         validate: validateColor
     }
 ];
 
 function init() {
     inquirer.prompt(questions).then((answers) => {
-        generateSVG(answers);
+        writeToFile(answers.text, generateSVG(answers));
+    });
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(`./${fileName}_logo.svg`, data, (err) => {
+        if (err) throw err;
+    
+        console.log('Logo created successfully!');
     });
 }
 
@@ -46,154 +55,11 @@ function validateColor(input) {
         return true;
     } else if (input.length == 6 && /^[0-9A-Fa-f]+$/.test(input)) {
         return true;
+    } else if (input.toLowerCase() === 'random') {
+        return true;
     }
     
-    return 'Must enter a color keyword (i.e. Red) or a valid 6 digit hexadecimalnumber (i.e. #a1b2c3) - try again'.brightRed;
+    return 'Must enter a color keyword (i.e. Red) or a valid 6 digit hexadecimal number (i.e. #a1b2c3) - try again'.brightRed;
 }
 
 init();
-
-validColors = [
-    "transparent",
-    "aliceblue",
-    "antiquewhite",
-    "aqua",
-    "aquamarine",
-    "azure",
-    "beige",
-    "bisque",
-    "black",
-    "blanchedalmond",
-    "blue",
-    "blueviolet",
-    "brown",
-    "burlywood",
-    "cadetblue",
-    "chartreuse",
-    "chocolate",
-    "coral",
-    "cornflowerblue",
-    "cornsilk",
-    "crimson",
-    "cyan",
-    "darkblue",
-    "darkcyan",
-    "darkgoldenrod",
-    "darkgray",
-    "darkgreen",
-    "darkkhaki",
-    "darkmagenta",
-    "darkolivegreen",
-    "darkorange",
-    "darkorchid",
-    "darkred",
-    "darksalmon",
-    "darkseagreen",
-    "darkslateblue",
-    "darkslategray",
-    "darkturquoise",
-    "darkviolet",
-    "deeppink",
-    "deepskyblue",
-    "dimgray",
-    "dodgerblue",
-    "firebrick",
-    "floralwhite",
-    "forestgreen",
-    "fuchsia",
-    "gainsboro",
-    "ghostwhite",
-    "gold",
-    "goldenrod",
-    "gray",
-    "green",
-    "greenyellow",
-    "honeydew",
-    "hotpink",
-    "indianred",
-    "indigo",
-    "ivory",
-    "khaki",
-    "lavender",
-    "lavenderblush",
-    "lawngreen",
-    "lemonchiffon",
-    "lightblue",
-    "lightcoral",
-    "lightcyan",
-    "lightgoldenrodyellow",
-    "lightgray",
-    "lightgreen",
-    "lightpink",
-    "lightsalmon",
-    "lightseagreen",
-    "lightskyblue",
-    "lightslategray",
-    "lightsteelblue",
-    "lightyellow",
-    "lime",
-    "limegreen",
-    "linen",
-    "magenta",
-    "maroon",
-    "mediumaquamarine",
-    "mediumblue",
-    "mediumorchid",
-    "mediumpurple",
-    "mediumseagreen",
-    "mediumslateblue",
-    "mediumspringgreen",
-    "mediumturquoise",
-    "mediumvioletred",
-    "midnightblue",
-    "mintcream",
-    "mistyrose",
-    "moccasin",
-    "navajowhite",
-    "navy",
-    "oldlace",
-    "olive",
-    "olivedrab",
-    "orange",
-    "orangered",
-    "orchid",
-    "palegoldenrod",
-    "palegreen",
-    "paleturquoise",
-    "palevioletred",
-    "papayawhip",
-    "peachpuff",
-    "peru",
-    "pink",
-    "plum",
-    "powderblue",
-    "purple",
-    "rebeccapurple",
-    "red",
-    "rosybrown",
-    "royalblue",
-    "saddlebrown",
-    "salmon",
-    "sandybrown",
-    "seagreen",
-    "seashell",
-    "sienna",
-    "silver",
-    "skyblue",
-    "slateblue",
-    "slategray",
-    "snow",
-    "springgreen",
-    "steelblue",
-    "tan",
-    "teal",
-    "thistle",
-    "tomato",
-    "turquoise",
-    "violet",
-    "wheat",
-    "white",
-    "whitesmoke",
-    "yellow",
-    "yellowgreen"
-]
--- utils/generateSVG.js
+++ utils/generateSVG.js
@@ -1,6 +1,19 @@
+const validColors = require('../colors.json');
+
 function generateSVG(answers) {
     console.log(answers);
     console.log('SVG Generated!');
+
+    if (answers.color.toLowerCase() === 'random') {
+        answers.color = validColors[Math.floor(Math.random() * validColors.length)];
+    }
+
+    if (answers.shapeColor.toLowerCase() === 'random') {
+        answers.shapeColor = validColors[Math.floor(Math.random() * validColors.length)];
+    }
+
+    console.log(answers.color);
+    console.log(answers.shapeColor);
 }
 
-module.exports = generateSVG
+module.exports = generateSVG;
