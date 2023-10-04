4156b4dba0e5de203281dff314a41bc398b30f4c
Hunter <muratoreh@gmail.com>
2023-10-04 10:12:26 -0400

    Completed unit testing - all passed

--- utils/shapes.test.js
+++ utils/shapes.test.js
@@ -1,26 +1,42 @@
-const generateSVG = require('./shapes');
+const {Circle, Square, Triangle} = require('./shapes.js');
 
-describe('Generate SVG Tests', () => {
-    it('Should return a string', () => {
-        const svg = generateSVG({
-            text: 'HMM',
-            color: 'blue',
-            shape: 'Circle',
-            shapeColor: 'green'
-        });
+describe('Shape Tests', () => {
+    it('Should return a Shape object', () => {
+        const circle = new Circle();
+        const square = new Square();
+        const triangle = new Triangle();
 
-        expect(typeof svg).toBe('string');
+        expect(circle).toBeInstanceOf(Circle);
+        expect(square).toBeInstanceOf(Square);
+        expect(triangle).toBeInstanceOf(Triangle);
     });
 
-    it('Should set random colors', () => {
-        const svg = generateSVG({
-            text: 'HMM',
-            color: 'random',
-            shape: 'Circle',
-            shapeColor: 'random'
-        });
+    it('Should set color to blue', () => {
+        const shape = new Circle();
 
-        expect(svg.color).not.toEqual('random');
-        expect(svg.shapeColor).not.toEqual('random');
+        shape.setColor('blue')
+
+        expect(shape.color).toEqual('blue');
+    });
+
+    it('Should render the circle svg string', () => {
+        const shape = new Circle();
+        shape.setColor("blue");
+
+        expect(shape.render()).toEqual(`<circle cx="150" cy="100" r="100" fill="blue"/>`);
+    });
+
+    it('Should render the square svg string', () => {
+        const shape = new Square();
+        shape.setColor("blue");
+
+        expect(shape.render()).toEqual(`<rect width="300" height="200" fill="blue"/>`);
+    });
+
+    it('Should render the triangle svg string', () => {
+        const shape = new Triangle();
+        shape.setColor("blue");
+
+        expect(shape.render()).toEqual(`<polygon points="150, 18 244, 182 56, 182" fill="blue"/>`);
     });
 })
